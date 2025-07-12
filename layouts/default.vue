149aba211965998a16c6ec088e6245e20aa80ef1
yareach8345 <mine8345@naver.com>
2025-07-12 23:55:02 +0900

    feature - 배경화면을 브라우저 전 화면에 적용

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="bg-chzzk-black text-white p-4">
+  <div class="bg-chzzk-black text-white p-4 min-h-svh">
     <header class="flex justify-center items-center w-full">
       <img src="/image/chzzk_icon.png" alt="치지직 로고" class="h-14"/>
       <div class="font-blackHan leading-none relative top-[2px] text-center text-4xl text-chzzk-neon-green">
