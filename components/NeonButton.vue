1dc18a365e807bf9a6292c84c801df76ad7b26a7
yareach8345 <mine8345@naver.com>
2025-07-13 18:32:25 +0900

    feature - 로그인시에만 페이지로 이동할 수 있게함
    - neon-button에 disabled 속성을 추가
    - 로그인이 안되어있을 시 sidebar의 아이템이 버튼을 disabled시킴
    - channels, devices, events-view 페이지에 미들웨어를 설정하여 로그인 시에만 접근할 수 있게함있

--- components/NeonButton.vue
+++ components/NeonButton.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+interface Props {
+  disabled?: boolean
+}
 
+const { disabled } = withDefaults(defineProps<Props>(), {
+  disabled: false
+})
 </script>
 
 <template>
-  <button class="border-4 border-chzzk-border hover:border-white hover:bg-selected rounded-lg transition-colors duration-500 ease-in-out p-2">
+  <button
+      class="border-4 border-chzzk-border rounded-lg transition-colors duration-500 ease-in-out p-2"
+      :class="{
+        'hover:border-white': !disabled,
+        'hover:bg-selected': !disabled,
+        'opacity-50': disabled
+      }"
+  >
     <slot/>
   </button>
 </template>
--- components/sidebar/PageNavItem.vue
+++ components/sidebar/PageNavItem.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import type { RouteLocationRaw } from '#vue-router'
+import { useAuthInfoStore } from '~/store/AuthInfoStore'
 
 interface Props {
   page: RouteLocationRaw | string
 }
 
 const { page } = defineProps<Props>()
+
+const { isAuthenticated } = storeToRefs(useAuthInfoStore())
+
+const disabled = computed(() => !isAuthenticated.value)
 
 const route: RouteLocationRaw = typeof(page) !== 'string'
   ? page
@@ -20,6 +25,7 @@ const onClick = () => {
   <neon-button
       class="flex gap-3 border-none rounded-none w-full"
       @click="onClick"
+      :disabled="disabled"
   >
     <slot name="icon"/>
     <slot/>
